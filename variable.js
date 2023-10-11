9b7a0aece074f4bcbd593ed14345ccde132dd2e4
Logic-Dell <inteldell296@gmail.com>
2023-10-11 02:56:30 -0700

    function hoisting

--- variable.js
+++ variable.js
@@ -1,18 +1,32 @@
-// var
-var carname; // declaring a variable
-carname = "benz"; // assigning a value
+// // var
+// var carname; // declaring a variable
+// carname = "benz"; // assigning a value
 
-var num = 5; // declaring and assigning
-num = 6; // re-assigning a new value
+// var num = 5; // declaring and assigning
+// num = 6; // re-assigning a new value
 
-name = "henry"; // declaring and assigning without var
-
-
-// let
-let animal = dog; // declaring a variable and assigning a value
-animal = cat; // assigning a new value
+// name = "henry"; // declaring and assigning without var
 
 
-// const
-const number = 67; 
-// a new value can not be assigned to the variable number because of the const keyword
+// // let
+// let animal = dog; // declaring a variable and assigning a value
+// animal = cat; // assigning a new value
+
+
+// // const
+// const number = 67; 
+// // a new value can not be assigned to the variable number because of the const keyword
+
+
+//hoisting
+console.log(addNumbers(4,5));
+
+function addNumbers(a,b) {
+    return a + b;
+}
+
+function subtractNumber(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subtractNumber(9,10));
