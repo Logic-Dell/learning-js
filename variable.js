32a1a485e41b2447b5aef2869dcf61a32e162d3d
Logic-Dell <inteldell296@gmail.com>
2023-10-11 02:30:20 -0700

    using the var to declare variables

---
+++ variable.js
@@ -0,0 +1,8 @@
+// var
+var carname; // declaring a variable
+carname = "benz"; // assigning a value
+
+var num = 5; // declaring and assigning
+num = 6; // re-assigning a new value
+
+name = "henry"; // declaring and assigning without var
