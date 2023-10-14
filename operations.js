4d0247fa72b47825b68d8bdb23134066822e919b
Logic-Dell <inteldell296@gmail.com>
2023-10-13 19:23:16 -0700

    operators in javascript

---
+++ operations.js
@@ -0,0 +1,35 @@
+let count = 9;
+
+// arithmetic operators
+count = count + 1; // 10
+count = count - 2; // 8
+count = count * 1; // 8
+count = count / 4; // 2
+count = count++; // 2
+count = ++count; // 4
+count = count--; // 4
+count = --count; // 2
+count = count ** 3; // 8
+
+// comparison operators(true/false)
+count == 0; // false
+count != 0; // true
+count < 0; // false
+count > 0; // true
+count <= 0; // false
+count >= 0; // true
+count === 0; // this checks if they are of the same data type and value
+count !== 0;
+
+// logical operators
+count > 0 && count < 5; // both have to be true
+count > 0 || count < 5; // atleast one has to be true
+
+
+// assignment operators
+count = 0; // 0
+count += 2; // 2
+count -= 1; // 1
+count /= 1; // 1
+count *= 2; // 2
+count %= 1; // 2
